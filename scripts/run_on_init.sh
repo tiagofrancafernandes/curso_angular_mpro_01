20afd689c3f7d07bcbdba27bb8cf9802fe80f153
Tiago França <tiago_pvo@hotmail.com>
2019-10-29 12:16:51 -0300

    Update run_on_init.sh

--- scripts/run_on_init.sh
+++ scripts/run_on_init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo "Ok, funcionou" > ./okfuncionou.txt
-#cd .. && ln -s $(pwd)/.profile_files/.bash_aliases ~/.bash_aliases && source ~/.bashrc
+DATETIME=$(date +"%Y-%m-%d_%M-%S")
+echo -e "Ok, funcionou\n$DATETIME" > ./STARTED_$DATETIME.txt
+cd .. && ln -s $(pwd)/.profile_files/.bash_aliases ~/.bash_aliases && source ~/.bashrc
