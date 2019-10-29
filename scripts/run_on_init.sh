8e5399844bc0799c7a406f6446ce368cb4edc86d
Tiago França <tiago_pvo@hotmail.com>
2019-10-29 15:58:30 -0300

    Update run_on_init.sh

--- scripts/run_on_init.sh
+++ scripts/run_on_init.sh
@@ -4,6 +4,8 @@ echo -e "Ok, funcionou\n$DATETIME" > ./STARTED_$DATETIME.txt
 
 if [ ! -f ~/.bash_aliases ]; then
   ln -s $GITPOD_REPO_ROOT/.profile_files/.bash_aliases ~/.bash_aliases
+  source ~/.bash_aliases
 fi
 
+sleep 5s
 source ~/.bashrc
