aafc1f01d5d51cd5a3b9432f0dd6eca8e8f6c072
Tiago França <tiago_pvo@hotmail.com>
2019-10-29 21:25:09 +0000

    Resolvendo .bash_aliases e run_on_init.sh

--- .profile_files/.bash_aliases
+++ .profile_files/.bash_aliases
@@ -1,2 +1,9 @@
 alias v='gp open'
 alias las='ls -lahs --color=auto'
+
+## GIT ALIAS ##
+alias gitstatus='git status'
+alias gitdiff='git diff'
+
+#mongo-express
+alias start-mongo-express='mongo-express -H 127.0.0.1 & mongod'
--- scripts/run_on_init.sh
+++ scripts/run_on_init.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 DATETIME=$(date +"%Y-%m-%d_%M-%S")
+BASHBIN=$(which bash)
 echo -e "Ok, funcionou\n$DATETIME" > ./STARTED_$DATETIME.txt
 
-source ../.profile_files/.bash_aliases
+echo "source $GITPOD_REPO_ROOT/.profile_files/.bash_aliases" >> $HOME/.bashrc
 
-sleep 5s
-source ~/.bashrc
-
-alias start-mongo-express='mongo-express -H 127.0.0.1 & mongod'
+$BASHBIN --rcfile $HOME/.bashrc
 
 #mongo-express -H 127.0.0.1 &
 #mongod &
