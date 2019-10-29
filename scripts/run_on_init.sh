15250fc4968eb9eb089e71af0a393983b3cdfec2
Tiago França <tiago_pvo@hotmail.com>
2019-10-29 16:00:32 -0300

    Update run_on_init.sh

--- scripts/run_on_init.sh
+++ scripts/run_on_init.sh
@@ -2,6 +2,8 @@
 DATETIME=$(date +"%Y-%m-%d_%M-%S")
 echo -e "Ok, funcionou\n$DATETIME" > ./STARTED_$DATETIME.txt
 
+source $GITPOD_REPO_ROOT/.profile_files/.bash_aliases
+
 if [ ! -f ~/.bash_aliases ]; then
   ln -s $GITPOD_REPO_ROOT/.profile_files/.bash_aliases ~/.bash_aliases
   source ~/.bash_aliases
